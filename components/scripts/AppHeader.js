c2126a3bec1a9c2ee6a515dd67f84d423f43632f
JeanC4rlo <jeancs.santos1@gmail.com>
2026-02-13 10:48:19 -0300

    Atualizando AppHeader e adicionando logo

--- components/scripts/AppHeader.js
+++ components/scripts/AppHeader.js
@@ -1,30 +1,27 @@
+import { AssetResolver } from "../../game/src/core/utils/AssetResolver.js";
+import { CSSFileLoader } from "../../game/src/core/utils/CSSFileLoader.js";
+
 class AppHeader extends HTMLElement {
     static tag = "app-header";
 
     async connectedCallback() {
 
-        // Define root dependendo do ambiente
-        const isGithubPages = window.location.hostname === "jprojetos.github.io";
-        const root = isGithubPages
-            ? "/maskeria/"
-            : "/";
+        CSSFileLoader.load(AssetResolver.from("components", `styles/${AppHeader.tag}.css`));
 
-        if (!document.querySelector(`link[data-component="${AppHeader.tag}"]`)) {
-            let link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = `${root}components/styles/${AppHeader.tag}.css`;
-            link.dataset.component = AppHeader.tag;
-            document.head.appendChild(link);
-        }
+        let logo;
+        if(this.classList.contains("dark") || this.classList.contains("transparent-dark"))
+            logo = AssetResolver.from("public", "logo/dark-border.svg");
+        else
+            logo = AssetResolver.from("public", "logo/light-border.svg");
 
         this.innerHTML = `
         <header>
-            <a class="home" href="${root}index.html">
-                <img src="" alt="Logo do Maskeria">
+            <a class="home" href="${AssetResolver.resolve("index.html")}">
+                <img src="${logo}" alt="Logo do Maskeria">
             </a>
             <nav>
-                <a class="wiki" href="${root}wiki.html">Wiki</a>
-                <a class="play" href="${root}play.html">Jogar</a>
+                <a class="wiki" href="${AssetResolver.resolve("wiki.html")}">Wiki</a>
+                <a class="play" href="${AssetResolver.resolve("play.html")}">Jogar</a>
             </nav>
         </header>
         `;
